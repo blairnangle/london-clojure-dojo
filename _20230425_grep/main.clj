259e1fbd56127e5c2822433f9441f363150bc2a3
Martin Roberts <martin.m.roberts@ibm.com>
2023-04-26 11:12:37 +0100

    Changes to return file details

--- _20230425_grep/main.clj
+++ _20230425_grep/main.clj
@@ -1,26 +1,36 @@
 (require '[babashka.fs :as fs]
          '[clojure.tools.cli :refer [parse-opts]])
 
-(println (fs/directory? "."))
-
 (def cli-options
 
   [["-f" "--file FILE" "File to read"]
-   ["-s" "--string STR" "String to find to read"]])
+   ["-s" "--string STR" "String to find to read"]
+   ["-r" "--root ROOT" "Root directory for search" :default "."]])
 
 (def options (:options (parse-opts *command-line-args* cli-options)))
 
 (defn filter-grab-line-number [found]
-      (filter #(when (some? %) (.indexOf found)) found))
+  (filter #(when (some? %) (.indexOf found)) found))
 
 (defn grep-file [file string]
-      (let [lines (fs/read-all-lines file)
-            found (map #(re-find (re-pattern string) %) lines)]
-           (loop [l lines f found r []]
-                 (recur (rest l) (rest f) (if (some? f)
-                                            (conj r {:line l})
-                                            r)))))
+  (let [lines (fs/read-all-lines file)
+        found (map #(re-find (re-pattern string) %) lines)
+        zmap (map vector (range 1 (inc (count lines))) lines found)]
+    (filter #(not (nil? (last %))) zmap)))
+
+
+(defn grep-files
+  [file string root]
+  (for [f (fs/glob root file)
+        :let [text (slurp (str f))
+              ;x (prn text)
+              ]
+        :when (re-find (re-pattern string) text)]
+    {:file (str f) :matches (grep-file f string)}))
 
 
 
-(prn (map #(grep-file % (:string options)) (fs/glob "." (:file options))))
+(prn (grep-files (:file options) (:string options) (:root options)))
+
+
+
